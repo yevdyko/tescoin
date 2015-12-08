1c28d1d9ac8adb2b35ddd5237c0013b71ee6cd8d
Robert Youngman <rob_youngman@yahoo.com>
2015-12-08 10:37:45 +0000

    tests search items

--- test/unit/controllers/tescoinSearchController.spec.js
+++ test/unit/controllers/tescoinSearchController.spec.js
@@ -7,4 +7,22 @@ describe('TescoinSearchController', function() {
 		ctrl = $controller('TescoinSearchController');
 	}));
 
+  it( 'initialises with an empty search result and term', function() {
+    expect(ctrl.searchResult).toBeUndefined();
+    expect(ctrl.searchTerm).toBeUndefined();
+  });
+
+  describe('when searching for a product', function() {
+    var items = [
+      {
+        "ImagePath": "http://img.tesco.com/Groceries/pi/642/5053526772642/IDShot_90x90.jpg",
+        "Name": "Tesco Everyday Value Sponge Pan Cleaners 8 Pack",
+        "Price": 0.4
+      }
+    ];
+
+    it('displays search results', function() {
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
+  });
 });
